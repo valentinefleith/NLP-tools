7a2961ce777ccbfb6dbab079e4e6d89758bf1c85
valentinefleith <valentine.fleith@gmail.com>
2023-10-17 17:46:31 +0200

    add main

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from Text import Text
+
+
+import sys
+
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Usage : py main.py /path/to/file.txt")
+    text = Text(sys.argv[1])
+    print(text.get_cleaned_lemmas())
