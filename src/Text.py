2c71f92c130aa3a21feabb959b197d4a6fbc1171
valentinefleith <valentine.fleith@gmail.com>
2023-10-17 17:44:39 +0200

    change names and use replace method

--- src/Text.py
+++ src/Text.py
@@ -5,21 +5,21 @@ class Text:
     def __init__(self, path):
         self.raw = Text.load_file(path)
 
-    def get_lemmatized(self):
+    def get_lemmas(self):
         raw_text = self.raw
         load = spacy.load("fr_core_news_sm", disable=["parser", "ner"])
         doc = load(raw_text)
         lemmatized = ' '.join(token.lemma_ for token in doc)
         return lemmatized
 
-    def get_cleaned(self):
+    def get_cleaned_lemmas(self):
         cleaned = ""
-        for char in self.get_lemmatized():
-            if char == "\n":
-                cleaned += " "
+        for char in self.get_lemmas():
+            #if char == "\n":
+               #cleaned += " "
             if char not in "?!():;.,«»–¬—*":
                 cleaned += char.lower()
-        return cleaned
+        return cleaned.replace("\n", " ").lower()
 
     @staticmethod
     def load_file(path):
