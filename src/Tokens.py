a6d5fa0dc7d97dd69923d99ccae32cc45dc11fa5
valentinefleith <valentine.fleith@gmail.com>
2023-10-21 00:58:19 +0200

    add methods for occurrence dictionaries

--- src/Tokens.py
+++ src/Tokens.py
@@ -5,6 +5,8 @@ class Tokens:
     def __init__(self, text: Text):
         self.tokens = Tokens.tokenize(text)
         self.lemmas = text.get_lemmatized_and_cleaned().split()
+        self.occ_dict = self.get_occ_dict()
+        self.occ_dict_without_stopwords = self.remove_empty_words()
 
     @staticmethod
     def tokenize(text: Text) -> list[str]:
@@ -28,3 +30,25 @@ class Tokens:
         for i in indices:
             words.pop(i)
         return words + apostrophes
+
+    def get_occ_dict(self):
+        occurrences_dict = {}
+        for element in self.tokens:
+            if element in occurrences_dict:
+                occurrences_dict[element] += 1
+            else:
+                occurrences_dict[element] = 1
+        return Tokens.sort_dict(occurrences_dict)
+
+    def remove_empty_words(self):
+        path = "aux/stop_words.txt"
+        with open(path, "r") as empty_words:
+            stop_words = empty_words.read()
+            new_dict = {key: self.occ_dict[key] for key in self.occ_dict if key not in stop_words}
+        return new_dict
+
+    @staticmethod
+    def sort_dict(dictionary):
+        sorted_keys = sorted(dictionary, key=dictionary.get)
+        sorted_dict = {key: dictionary[key] for key in sorted_keys}
+        return sorted_dict
