381f72101a1a124f7ac576bf1ede05f42dcd0c48
valentinefleith <valentine.fleith@gmail.com>
2023-10-21 23:42:14 +0200

    remove unused modules

--- src/lemmatization.py
+++
@@ -1,8 +0,0 @@
-import spacy
-
-
-def lemmatize(text):
-    load = spacy.load("fr_core_news_sm", disable=["parser", "ner"])
-    doc = load(text)
-    final_string = ' '.join(token.lemma_ for token in doc)
-    return final_string
