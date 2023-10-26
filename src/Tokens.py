4f42956ca3b45f7208e35cd22152369a93b52cf0
valentinefleith <valentine.fleith@gmail.com>
2023-10-26 11:55:34 +0200

    add type hints

--- src/Tokens.py
+++ src/Tokens.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+
 from Text import Text
 
 
@@ -21,15 +23,15 @@ class Tokens:
         self.lemmas = text.get_lemmatized_and_cleaned().split()
 
     @staticmethod
-    def tokenize(text: Text) -> list[str]:
+    def tokenize(text: Text) -> List[str]:
         """Takes in a text, returns a list of all its words."""
         tokens = text.get_raw_cleaned().split()
         return tokens
 
-    def get_occ_dict(self):
+    def get_occ_dict(self) -> Dict[str, int]:
         return Tokens.create_occ_dict(self.tokens)
 
-    def get_occ_dict_without_stopwords(self, path_to_stopwords="aux/stop_words.txt"):
+    def get_occ_dict_without_stopwords(self, path_to_stopwords="aux/stop_words.txt") -> Dict[str, int]:
         """From a dict {word: occurences}, removes stop-words"""
         with open(path_to_stopwords, "r") as empty_words:
             stop_words = empty_words.read()
@@ -38,7 +40,7 @@ class Tokens:
         return new_dict
 
     @staticmethod
-    def create_occ_dict(tokens):
+    def create_occ_dict(tokens: List[str]) -> Dict[str, int]:
         """Takes in a list of tokens, returns a dict {word: occurences}"""
         occurrences_dict = {}
         for element in tokens:
